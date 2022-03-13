ba63705e67878e7624822916ac99590b88e63d2d
luka8094 <luka8094@stud.kea.dk>
2022-03-13 13:54:45 +0100

    feat: added menu underlay effect

--- public/components/menu.js
+++ public/components/menu.js
@@ -2,15 +2,18 @@ window.onload = () => {
 
     const menuSidebarHtmlElement = document.querySelector("#sidebar")
     const menuButtonHtmlElement = document.querySelector("#menu-button")
+    const menuUnderlayHtmlELement = document.querySelector("#menu-underlay")
 
     console.log(menuSidebarHtmlElement)
     console.log(menuButtonHtmlElement)
+    console.log(menuUnderlayHtmlELement)
 
     menuButtonHtmlElement.onclick = openMenu
 
     function openMenu(){
         menuButtonHtmlElement.classList.toggle("click")
         menuSidebarHtmlElement.classList.toggle("show")
+        menuUnderlayHtmlELement.classList.toggle("menu-fade")
     }
 
 }
